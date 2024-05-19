ac9ffad6923a549665a6abec7ebe634ac8205827
zianisamir <zianisamir5636789@gmail.com>
2024-05-19 15:31:54 +0100

    configure llm

---
+++ src/utils/config.py
@@ -0,0 +1,7 @@
+DOCUMENTS_PATH = "documents/"
+LLM_MODEL_FILE_PATH = "/home/samir/Desktop/projectllm/NockNockDoc/src/model-mistral/mistral-7b-instruct-v0.1.Q2_K.gguf"
+CONTEXT_SIZE = 8000
+MAX_REFERENCES = 4
+stop_words = [
+    "[USER]", "[Assistant]", "User:", "Assistant:" , "[/INST]", "[/SYS]", "<<SYS>>", "<</SYS>>", "<s>", "</s>", "[SYS]", "[INST]"
+]
