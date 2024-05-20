914312db8bb5b20e49ae7113702cda71d14596bd
zianisamir <zianisamir5636789@gmail.com>
2024-05-20 20:49:53 +0100

    adapter config path for llm

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,5 +1,5 @@
 DOCUMENTS_PATH = "documents/"
-LLM_MODEL_FILE_PATH = "/home/samir/Desktop/projectllm/NockNockDoc/src/model-mistral/mistral-7b-instruct-v0.1.Q2_K.gguf"
+LLM_MODEL_FILE_PATH = "src/model-mistral/mistral-7b-instruct-v0.1.Q2_K.gguf"
 CONTEXT_SIZE = 8000
 MAX_REFERENCES = 4
 stop_words = [
