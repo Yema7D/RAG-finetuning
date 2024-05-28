a46f32a64f34b20cc1f691e1f8c26e28208be3df
zianisamir <zianisamir5636789@gmail.com>
2024-05-28 12:07:02 +0100

    adapte prombt

--- src/utils/prompt.py
+++ src/utils/prompt.py
@@ -1,63 +1,61 @@
-def get_questions_from_content_prompt_mistral(context, nbr_questions=40, language="english"):
-        return f"""<s><<SYS>>
-You are a highly detailed-oriented Assistant.
-Your task is to generate unique and context-specific questions based on the provided text. These questions should be designed to explore every aspect of the context in depth.
-Guidelines for question formulation:
-1. Each question must directly relate to the information in the context.
-2. Avoid general or vague questions. Instead, tailor each question to specific details, names, events, or facts mentioned in the context.
-3. Ensure that the questions collectively cover all key elements of the context, including but not limited to names, dates, events, personal information, statistics, and any specific terminologies or concepts mentioned.
-4. Do not provide answers to these questions.
-5. The question should always include "What", "Who", "When", "Where" when possible.
+def get_questions_from_content_prompt_mistral(context, nbr_questions=40, language="french"):
+    return f"""<s><<SYS>>
+Tu es un Assistant très minutieux et détaillé.
+Ta tâche est de générer des questions uniques et spécifiques au contexte fourni, qui est axé sur la création d'histoires de jeux pédagogiques. Ces questions doivent être conçues pour explorer chaque aspect du contexte en profondeur.
+Directives pour la formulation des questions :
+1. Chaque question doit être directement liée aux informations du contexte.
+2. Évite les questions générales ou vagues. Au lieu de cela, adapte chaque question aux détails spécifiques, noms, événements ou faits mentionnés dans le contexte.
+3. Assure-toi que les questions couvrent tous les éléments clés du contexte, y compris, mais sans s'y limiter, les noms des personnages, les dates, les événements, les informations de jeu, les statistiques, et toute terminologie ou concept spécifique mentionné.
+4. Ne fournis pas de réponses à ces questions.
+5. Les questions doivent toujours inclure "Quoi", "Qui", "Quand", "Où" lorsque cela est possible.
 
-
-Examples of well-formulated questions:
-- Instead of "What is the birth date of the person?", ask "What is the birth date of [person's name] mentioned in the [describe the context]?"
-- Instead of "What is the email?", ask "What is the email of [person's name] mentioned in the [describe the context]?"
-- Instead of "What are the skills?", ask "What are the skills of [person's name] mentioned in the [describe the context]?"
-- Instead of "What are the expertise?", ask "What are the expertise of [person's name] mentioned in the [describe the context]?"
-- Instead of "What are the programming languages?", ask "what are the programming languages that [person's name] can use?"
-- Instead of "What are the qualities?", ask "What are the qualities of [person's name] mentioned in the [describe the context]?"
-- Replace "What is the system used?" with "What specific system is mentioned as being used in [describe the context]?"
-- Instead of "What is the name of the person?", ask "Who is the person referred to as [specific role or description] in the [describe the context]?"
-- Change "What is the end date of the project?" to "What is the projected completion date for [project name] as described in the [describe the context]?"
+Exemples de questions bien formulées :
+- Au lieu de "Quelle est la mission ?", demande "Quelle est la mission principale du personnage [nom du personnage] dans [décrire le contexte] ?"
+- Au lieu de "Quel est le défi ?", demande "Quel est le défi majeur que [nom du personnage] doit surmonter dans [décrire le contexte] ?"
+- Au lieu de "Quels sont les objectifs ?", demande "Quels sont les objectifs de [nom du personnage] dans [décrire le contexte] ?"
+- Au lieu de "Quels sont les obstacles ?", demande "Quels obstacles [nom du personnage] doit-il affronter dans [décrire le contexte] ?"
+- Au lieu de "Quels sont les alliés ?", demande "Qui sont les alliés de [nom du personnage] mentionnés dans [décrire le contexte] ?"
+- Au lieu de "Quel est l'environnement ?", demande "Dans quel environnement [nom du personnage] évolue-t-il dans [décrire le contexte] ?"
+- Remplace "Quel est l'outil utilisé ?" par "Quel outil spécifique [nom du personnage] utilise-t-il dans [décrire le contexte] ?"
+- Au lieu de "Qui est l'ennemi ?", demande "Qui est l'ennemi principal de [nom du personnage] dans [décrire le contexte] ?"
+- Change "Quelle est la date de début de la mission ?" en "Quand commence la mission de [nom du personnage] dans [décrire le contexte] ?"
 </s><</SYS>>
 
-Context:
+Contexte :
 ```
 {context}
 ```
 
-User : Based on the above context, please generate a list of exactly {nbr_questions} distinct and contextually relevant questions. Ensure that these questions comprehensively cover the provided context.
-Don't never never ask general or vague questions.
-Always add "What", "Who", "When", "Where" when possible.
-Do not end the question with phrase like "in the context" or "in the given context" or "dans le contexte" or similar.
-Give a list of exactly {nbr_questions} questions.
-Give me the questions in {language} language.
+Utilisateur : Basé sur le contexte ci-dessus, veuillez générer une liste de exactement {nbr_questions} questions distinctes et contextuellement pertinentes. Assurez-vous que ces questions couvrent de manière exhaustive le contexte fourni.
+Ne posez jamais de questions générales ou vagues.
+Ajoutez toujours "Quoi", "Qui", "Quand", "Où" lorsque possible.
+Ne terminez pas la question avec des phrases comme "dans le contexte" ou "dans le contexte donné" ou "dans le contexte" ou similaire.
+Donnez-moi les questions en {language}.
 Assistant: """
 
 
-def get_answer_from_content_prompt_mistral(context, question, language="english"):
-        print("question", question)
-        return f"""<s><<SYS>>
-You are a highly detail-oriented Assistant tasked with generating professional and detailed answers. Your responses should be based strictly on the provided context and in the specified language. Do not use external knowledge or information not included in the context.
+def get_answer_from_content_prompt_mistral(context, question, language="french"):
+    print("question", question)
+    return f"""<s><<SYS>>
+Tu es un Assistant très minutieux et détaillé. Ta tâche est de générer des réponses professionnelles et détaillées, spécifiques au domaine des jeux pédagogiques. Tes réponses doivent être strictement basées sur le contexte fourni et dans la langue spécifiée. N'utilise pas de connaissances ou d'informations externes non incluses dans le contexte.
 </s><</SYS>>
 
-Context:
+Contexte :
 
 {context}
 
+Utilisateur : Veuillez répondre à la question suivante en {language} : "{question}".
+Si le contexte ne fournit pas suffisamment d'informations, réponds par : "Je n'ai pas assez de contenu pour répondre à cette question."
 
-User: Please answer the following question in {language}: "{question}".
-If the context does not provide enough information, respond with: "I do not have enough content to answer this question."
-
-[Reminder: The response must be in {language}.]
+[Rappel : La réponse doit être en {language}.]
 
 Assistant: """
 
 
-def get_questions_from_content(context, nbr_questions=30, model="mistral", language="english"):
-        return get_questions_from_content_prompt_mistral(context, nbr_questions, language)
+def get_questions_from_content(context, nbr_questions=30, model="pedagogic_game", language="french"):
+    return get_questions_from_content_prompt_mistral(context, nbr_questions, language)
 
 
-def get_answer_question_from_content(context, question, model="mistral"):
-        return get_answer_from_content_prompt_mistral(context, question, language="english")
+
+def get_answer_question_from_content(context, question, model="pedagogic_game"):
+    return get_answer_from_content_prompt_mistral(context, question, language="french")
